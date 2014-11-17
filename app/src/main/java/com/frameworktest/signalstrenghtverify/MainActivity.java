d5b16e731457e75112930d2cdfbf8651d8d27f98
ericpoof <ericpoof@gmail.com>
2014-11-16 21:55:34 -0800

    getting valid values from PhoneStateListener

--- app/src/main/java/com/frameworktest/signalstrenghtverify/MainActivity.java
+++ app/src/main/java/com/frameworktest/signalstrenghtverify/MainActivity.java
@@ -1,8 +1,11 @@
 package com.frameworktest.signalstrenghtverify;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -52,6 +55,7 @@ public class MainActivity extends Activity {
     private void measureSignalStrength() {
         SignalStrength signalStrength = new SignalStrength();
         int gsm_rssi, umts_rscp, lte_rsrp;
+        String updateMsg = "";
         gsm_rssi = signalStrength.getGsmSignalStrength();
         umts_rscp = signalStrength.getGsmSignalStrength();
         lte_rsrp = signalStrength.getLteRsrp();
@@ -60,13 +64,67 @@ public class MainActivity extends Activity {
         Log.i(TAG, "umtis_rscp = " + umts_rscp);
         Log.i(TAG, "lte_rsrp = " + lte_rsrp);
 
+        MyPhoneStateListener myListener = new MyPhoneStateListener();
+        TelephonyManager mTelephonyManager  = ( TelephonyManager )getSystemService(Context.TELEPHONY_SERVICE);
+        mTelephonyManager.listen(myListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+
+
+        if (mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_CDMA) {
+            updateMsg = "cdma dBM=" + signalStrength.getCdmaDbm();
+        } else if  (mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+            updateMsg = "gsm signal=" + signalStrength.getGsmSignalStrength();
+        }
+
+        /*
         TextView gsm_value_box = (TextView) findViewById(R.id.gsm_rssi);
         TextView umts_value_box = (TextView) findViewById(R.id.umts_rscp);
         TextView lte_value_box = (TextView) findViewById(R.id.lte_rsrp);
-        gsm_value_box.setText(Integer.toString(gsm_rssi));
+        gsm_value_box.setText(updateMsg);
         umts_value_box.setText(Integer.toString(umts_rscp));
         lte_value_box.setText(Integer.toString(lte_rsrp));
+        */
 
+    }
+
+    private class MyPhoneStateListener extends PhoneStateListener{
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength){
+            super.onSignalStrengthsChanged(signalStrength);
+            Log.i(TAG, "onSignalStrengthsChanged: " + signalStrength);
+            if (signalStrength.isGsm()) {
+                Log.i(TAG, "onSignalStrengthsChanged: getGsmBitErrorRate "
+                        + signalStrength.getGsmBitErrorRate());
+                Log.i(TAG, "onSignalStrengthsChanged: getGsmSignalStrength "
+                        + signalStrength.getGsmSignalStrength());
+            } else if (signalStrength.getCdmaDbm() > 0) {
+                Log.i(TAG, "onSignalStrengthsChanged: getCdmaDbm "
+                        + signalStrength.getCdmaDbm());
+                Log.i(TAG, "onSignalStrengthsChanged: getCdmaEcio "
+                        + signalStrength.getCdmaEcio());
+            } else {
+                Log.i(TAG, "onSignalStrengthsChanged: getEvdoDbm "
+                        + signalStrength.getEvdoDbm());
+                Log.i(TAG, "onSignalStrengthsChanged: getEvdoEcio "
+                        + signalStrength.getEvdoEcio());
+                Log.i(TAG, "onSignalStrengthsChanged: getEvdoSnr "
+                        + signalStrength.getEvdoSnr());
+            }
+
+            Log.i(TAG, "onSignalStrengthsChanged: LteSignalStrength " + signalStrength.getLteSignalStrength());
+            Log.i(TAG, "onSignalStrengthsChanged: getLteRsrp " + signalStrength.getLteRsrp());
+            Log.i(TAG, "onSignalStrengthsChanged: getLteRsrq " + signalStrength.getLteRsrq());
+            Log.i(TAG, "onSignalStrengthsChanged: getLteRsssnr " + signalStrength.getLteRssnr());
+            Log.i(TAG, "onSignalStrengthsChanged: getLteCqi " + signalStrength.getLteCqi());
+
+
+
+            TextView gsm_value_box = (TextView) findViewById(R.id.gsm_rssi);
+            TextView umts_value_box = (TextView) findViewById(R.id.umts_rscp);
+            TextView lte_value_box = (TextView) findViewById(R.id.lte_rsrp);
+            gsm_value_box.setText( "gsm " + signalStrength.getGsmSignalStrength());
+            umts_value_box.setText("lte signal " + signalStrength.getLteSignalStrength());
+            lte_value_box.setText("lte rsrp " + signalStrength.getLteRsrp());
+        }
     }
 
 
